0f6b062a5260f26a844b926fb17a2f74496a9427
AdrRnv <adrian.rabian@gmail.com>
2025-03-12 12:01:04 +0100

    feat(POT-0): Career

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,25 +79,6 @@ export default function Home() {
                   />
                 </svg>
               </button>
-              <button
-                className={`p-2 hover:${
-                  theme === "dark" ? "bg-[#2C2C2C]" : "bg-gray-100"
-                } rounded-lg transition-colors`}
-              >
-                <svg
-                  className="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
-                  />
-                </svg>
-              </button>
             </div>
             <div className="flex items-center gap-4">
               <button
@@ -257,19 +238,19 @@ export default function Home() {
               {[
                 {
                   name: "FiTool",
-                  description: "Nutrition and Session website",
+                  description: "Nutritional and training tracking website",
                   logo: "/assets/images/LogoFT.png",
                   color: "bg-white",
                 },
                 {
                   name: "FitPulse",
-                  description: "Nutrition and Session Mobile App",
-                  icon: "M",
+                  description: "Nutritional and training tracking mobile app",
+                  logo: "/assets/images/LogoFT.png",
                   color: "bg-white",
                 },
               ].map((project) => (
                 <button
-                  key={project.name}
+                  key={`${project.name}-${project.description}`}
                   className={`w-full ${
                     theme === "dark"
                       ? "bg-[#2C2C2C] hover:bg-[#363636]"
@@ -286,19 +267,136 @@ export default function Home() {
                           : "border-gray-200"
                       } ring-2 ring-black/5`}
                     >
-                      {project.logo ? (
-                        <div className="relative w-full h-full flex items-center justify-center">
-                          <Image
-                            src={project.logo}
-                            alt={`${project.name} logo`}
-                            width={28}
-                            height={28}
-                            className="object-contain drop-shadow-md"
-                          />
-                        </div>
-                      ) : (
-                        project.icon
-                      )}
+                      <div className="relative w-full h-full flex items-center justify-center">
+                        <Image
+                          src={project.logo}
+                          alt={`${project.name} logo`}
+                          width={28}
+                          height={28}
+                          className="object-contain drop-shadow-md"
+                        />
+                      </div>
+                    </div>
+                    <div className="text-left">
+                      <h3 className="font-medium">{project.name}</h3>
+                      <p
+                        className={
+                          theme === "dark" ? "text-gray-400" : "text-gray-500"
+                        }
+                      >
+                        {project.description}
+                      </p>
+                    </div>
+                  </div>
+                  <svg
+                    className={`w-6 h-6 ${
+                      theme === "dark"
+                        ? "text-gray-500 group-hover:text-white"
+                        : "text-gray-400 group-hover:text-black"
+                    } transition-colors`}
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div
+          className={`${
+            theme === "dark" ? "bg-[#1C1C1C]" : "bg-white"
+          } rounded-2xl p-4 shadow-lg mt-6`}
+        >
+          <div
+            className={`${
+              theme === "dark" ? "bg-[#1C1C1C]" : "bg-white"
+            } rounded-2xl`}
+          >
+            <div className="flex items-center justify-between mb-6">
+              <div className="flex items-center gap-2">
+                <span className="w-2 h-2 bg-gray-500 rounded-full"></span>
+                <span className="text-lg">Carrer</span>
+              </div>
+              <button
+                className={`${
+                  theme === "dark"
+                    ? "text-gray-400 hover:text-white"
+                    : "text-gray-500 hover:text-black"
+                } flex items-center gap-2 transition-colors`}
+              >
+                View All
+                <svg
+                  className="w-4 h-4"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M9 5l7 7-7 7"
+                  />
+                </svg>
+              </button>
+            </div>
+
+            <div className="space-y-4">
+              {[
+                {
+                  name: "EffetB",
+                  description: "Apprentice Full-Stack Developer",
+                  logo: "/assets/images/LogoFT.png",
+                  color: "bg-white",
+                },
+                {
+                  name: "EffetB",
+                  description: "Full-Stack Developer",
+                  logo: "/assets/images/LogoFT.png",
+                  color: "bg-white",
+                },
+                {
+                  name: "EffetB",
+                  description: "Intern Full-Stack Developer",
+                  logo: "/assets/images/LogoFT.png",
+                  color: "bg-white",
+                },
+              ].map((project) => (
+                <button
+                  key={`${project.name}-${project.description}`}
+                  className={`w-full ${
+                    theme === "dark"
+                      ? "bg-[#2C2C2C] hover:bg-[#363636]"
+                      : "bg-gray-100 hover:bg-gray-200"
+                  } rounded-xl p-4 flex items-center justify-between group transition-colors`}
+                >
+                  <div className="flex items-center gap-4">
+                    <div
+                      className={`w-12 h-12 ${
+                        project.color
+                      } rounded-full flex items-center justify-center text-xl overflow-hidden p-2 shadow-lg ${
+                        theme === "dark"
+                          ? "border-[#1C1C1C]"
+                          : "border-gray-200"
+                      } ring-2 ring-black/5`}
+                    >
+                      <div className="relative w-full h-full flex items-center justify-center">
+                        <Image
+                          src={project.logo}
+                          alt={`${project.name} logo`}
+                          width={28}
+                          height={28}
+                          className="object-contain drop-shadow-md"
+                        />
+                      </div>
                     </div>
                     <div className="text-left">
                       <h3 className="font-medium">{project.name}</h3>
